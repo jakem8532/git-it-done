82727cca89aac2be1103273311a813ae03ce2522
jakem8532 <85272755+jakem8532@users.noreply.github.com>
2022-06-02 14:20:03 -0400

    restructed fetch command

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,11 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos")
+var getUserRepos = function(user) {
+    var apiUrl = "https://api.github.com/users/" + user + "/repos"
+
+    fetch(apiUrl).then(function(res) {
+        res.json().then(function(data) {
+            console.log(data)
+        })
+    })
 }
 
-getUserRepos()
+getUserRepos("jakem8532")
